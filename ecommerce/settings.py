304212d2161d161f48ba309916803c618b3d3457
mdtalha178 <talhaazmi178@gmail.com>
2023-12-14 22:54:41 +0530

    django secret key remove from setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$jx5fqneqf3iqqbbf&jr*g*cm3^bl$k%ei&vt^j4x2n_d0n5=r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
